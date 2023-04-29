a567dd182eeeed96ffea551f43a4451329a6c69e
Rodrigo WIlliam Chavoni <rwchavoni@pop-os.localdomain>
2023-04-29 18:20:51 -0300

    ajustes de explicadores

--- id3_classification.py
+++ id3_classification.py
@@ -1,38 +1,60 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 import numpy as np
+import pandas as pd
+
+def loadData():
+    data = pd.read_csv('treino_sinais_vitais_com_label.txt',sep=',', header=None)
+    
+    data.columns = ["index","pSist", "pDiast", "qPA", "pulso", "respiração", "gravidade", "classificação"]
+    
+    data.drop(columns=['index','pSist','pDiast'], inplace=True)
+    
+    explicadores = data[['qPA','pulso','respiração']]
+    
+    target = data[['classificação']]
+    
+    print('Data shape:', data.shape)
+
+    return explicadores, target
+
+# def loadDataSet():
+#     # Open the text file and read the dataSet
+#     with open('treino_sinais_vitais_com_label.txt', 'r') as file:
+#         dataSet = file.readlines()
+
+#     # Remove any leading or trailing white space and split the dataSet by commas
+#     dataSet = [line.strip().split(',') for line in dataSet]
+
+#     # Convert the dataSet to a NumPy array
+#     dataSet = np.array(dataSet, dtype=np.float64)
+
+#     print(dataSet)
+#     # Exclui a coluna index
+#     dataSet = np.delete(dataSet, 0, 1)
+
+#     # Exclui a coluna gravidade
+#     dataSet = np.delete(dataSet, 5, 1)
 
 
-def loadDataSet():
-    # Open the text file and read the dataSet
-    with open('treino_sinais_vitais_com_label.txt', 'r') as file:
-        dataSet = file.readlines()
+#     # Extract the features (X) and target variable (y)
+#     targets = dataSet[:, -1]
 
-    # Remove any leading or trailing white space and split the dataSet by commas
-    dataSet = [line.strip().split(',') for line in dataSet]
+#     # Print the shape of the dataSet
+#     print('Data shape:', dataSet.shape)
 
-    # Convert the dataSet to a NumPy array
-    dataSet = np.array(dataSet, dtype=np.float64)
-
-    # Exclui a coluna index
-    dataSet = np.delete(dataSet, 0, 1)
-    # Exclui a coluna gravidade
-    dataSet = np.delete(dataSet, 5, 1)
-
-    # Extract the features (X) and target variable (y)
-    targets = dataSet[:, -1]
-
-    # Print the shape of the dataSet
-    print('Data shape:', dataSet.shape)
-
-    return dataSet, targets
+#     return dataSet, targets
 
 
 # Load dataset
-data, target = loadDataSet()
+explicadores, target = loadData()
+
+print(explicadores)
+print(target)
 
 # Split dataset into training and testing sets
-data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.3)
+data_train, data_test, target_train, target_test = train_test_split(explicadores, target, test_size=0.3)
 
 # Train the Decision Tree using ID3 algorithm
 tree = DecisionTreeClassifier(criterion="entropy")
@@ -40,6 +62,10 @@ tree.fit(data_train, target_train)
 
 # Predict on test set
 y_pred = tree.predict(data_test)
+
+cm = confusion_matrix(target_test, y_pred)
+
+print(cm)
 
 # Print accuracy score
 print("Accuracy:", tree.score(data_test, target_test))
--- id3_regression.py
+++ id3_regression.py
@@ -1,33 +1,49 @@
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 import numpy as np
+import pandas as pd
+
+def loadData():
+    data = pd.read_csv('treino_sinais_vitais_com_label.txt',sep=',', header=None)
+    
+    data.columns = ["index","pSist", "pDiast", "qPA", "pulso", "respiração", "gravidade", "classificação"]
+    
+    data.drop(columns=['index','pSist','pDiast'], inplace=True)
+    
+    explicadores = data[['qPA','pulso','respiração']]
+    
+    target = data[['gravidade']]
+    
+    print('Data shape:', data.shape)
+
+    return explicadores, target
 
 
-def loadDataSet():
-    # Open the text file and read the dataSet
-    with open('treino_sinais_vitais_sem_label.txt', 'r') as file:
-        dataSet = file.readlines()
+# def loadDataSet():
+#     # Open the text file and read the dataSet
+#     with open('treino_sinais_vitais_sem_label.txt', 'r') as file:
+#         dataSet = file.readlines()
 
-    # Remove any leading or trailing white space and split the dataSet by commas
-    dataSet = [line.strip().split(',') for line in dataSet]
+#     # Remove any leading or trailing white space and split the dataSet by commas
+#     dataSet = [line.strip().split(',') for line in dataSet]
 
-    # Convert the dataSet to a NumPy array
-    dataSet = np.array(dataSet, dtype=np.float64)
+#     # Convert the dataSet to a NumPy array
+#     dataSet = np.array(dataSet, dtype=np.float64)
 
-    # Exclui a coluna index
-    dataSet = np.delete(dataSet, 0, 1)
+#     # Exclui a coluna index
+#     dataSet = np.delete(dataSet, 0, 1)
 
-    # Extract the features (X) and target variable (y)
-    targets = dataSet[:, -1]
+#     # Extract the features (X) and target variable (y)
+#     targets = dataSet[:, -1]
 
-    return dataSet, targets
+#     return dataSet, targets
 
 
 # Load dataset
-data, target = loadDataSet()
+explicadores, target = loadData()
 
 # Split dataset into training and testing sets
-data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.3)
+data_train, data_test, target_train, target_test = train_test_split(explicadores, target, test_size=0.3)
 
 # Train the Decision Tree using ID3 algorithm
 tree = DecisionTreeRegressor()
